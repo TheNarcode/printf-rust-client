f31f731692ec52bfdea8bdeede5a4f97cabf6c77
aditya yadav <dedomil@tuta.io>
2025-08-31 15:09:48 +0530

    fix printermanager

--- src/client.rs
+++ src/client.rs
@@ -4,10 +4,9 @@ use crate::{
 };
 use eventsource_client::SSE;
 
-pub async fn got_event(event: SSE) {
+pub async fn got_event(event: SSE, pm: &mut PrinterManager) {
     if let SSE::Event(e) = event {
         if let "update" = e.event_type.as_str() {
-            let mut pm = PrinterManager::new("config.json");
             let json_string: String = serde_json::from_str(&e.data).unwrap();
             let attributes: PrintAttributes = serde_json::from_str(&json_string).unwrap();
             let printer = pm.get_next_printer(&attributes.color).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::sync::Arc;
+
 use crate::client::got_event;
+use crate::ipp::PrinterManager;
 use eventsource_client as es;
 use eventsource_client::Client;
 use futures::TryStreamExt;
+use tokio::sync::Mutex;
 
 pub mod client;
 pub mod ipp;
@@ -12,12 +16,17 @@ const URL: &str = "https://archlinux.234892.xyz/event/sse";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = es::ClientBuilder::for_url(URL)?.build();
+    let pm = Arc::new(Mutex::new(PrinterManager::new("config.json")));
 
     client
         .stream()
-        .try_for_each(|event| async {
-            got_event(event).await;
-            Ok(())
+        .try_for_each(|event| {
+            let pm = pm.clone();
+            async move {
+                let mut pm = pm.lock().await;
+                got_event(event, &mut pm).await;
+                Ok(())
+            }
         })
         .await?;
 
